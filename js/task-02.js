a42efe67f514e01fc62fff21d2222098babe82bc
Bohdan <nalivkin.bog@gmail.com>
2022-12-21 16:57:41 +0200

    1,2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+let foodList = document.getElementById('ingredients');
+const foodIngredients = ingredients.map((ingredient) => {
+  let item = document.createElement('li');
+  item.textContent = ingredient;
+  return item;
+  
+});
+foodList.append(...foodIngredients);
+
+console.log(foodIngredients);
